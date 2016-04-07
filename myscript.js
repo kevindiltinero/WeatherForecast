0b85548b3d497bf3a35c0f0c10669b51c717fa71
kevinfitzpatrick <mastery414@gmail.com>
2016-04-07 16:12:03 +0100

    This is the initial draft

--- myscript.js
+++ myscript.js
@@ -1,6 +1,16 @@
-function showMe (box) {
-    document.getElementById(box).style.display = 'inline';
-}
+$(document).ready(function(){
+    $('input[type="checkbox"]').click(function(){
+        if($(this).attr("value")=="red"){
+            $(".red").toggle();
+        }
+        if($(this).attr("value")=="green"){
+            $(".green").toggle();
+        }
+        if($(this).attr("value")=="blue"){
+            $(".blue").toggle();
+        }
+    });
+});
 
 
 function testResults (form) {
@@ -14,112 +24,138 @@ function testResults (form) {
     var units = form.units.value;
     var days = form.days.value;
 
-    //var url = "http://api.openweathermap.org/data/2.5/forecast?q="+location+"&mode=json&appid=dab71e5e8eb47351af0a7bdb5bcac16b&units="+units+"&cnt="+days;
-    //var url = "http://api.openweathermap.org/data/2.5/forecast?q="+location+"&mode=json&appid=dab71e5e8eb47351af0a7bdb5bcac16b&units="+units;
 
     var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+location+'&mode=json&appid=dab71e5e8eb47351af0a7bdb5bcac16b&units='+units+'&cnt='+days;
-    //var url = 'http://api.openweathermap.org/data/2.5/forecast?lat=53.3498053&lon=-6.260309699999993&mode=json&appid=5285c55183ef5de1fc376eb445326ff5&units=metric'
+    var codeurl = 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyD0B92s8QnfLcIV1kP7dn5vGv7ldKz4Q88';
 
-    //Standard Request given by W3
+
+     //Standard Request given by W3
     xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        myFunction(xmlhttp.responseText);
+        codeFunction(xmlhttp.responseText);
     }
     }
-    xmlhttp.open("GET", url, true);
+    xmlhttp.open("GET", codeurl, true);
     xmlhttp.send();
 
 
-    function myFunction(response) {
-        var data = JSON.parse(response) ;
+    //Function executed on it's behalf
+    function codeFunction(response) {
+        var otherdata = JSON.parse(response);
+        var errorcode = otherdata.status;
 
-        // GOOGLE MAP STUFF
-        var myCenter=new google.maps.LatLng(data.city.coord.lat,data.city.coord.lon);
-
-        var mapProp = {
-          center:myCenter,
-          zoom:5,
-          mapTypeId:google.maps.MapTypeId.ROADMAP
-          };
-
-        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-        var marker=new google.maps.Marker({
-          position:myCenter,
-          });
-
-        marker.setMap(map);
-
-        document.getElementById("Mapintro").innerHTML = "THis is where you picked";
-
-        //This is the city table
-        var out = "<table>";
-        out += "<tr><td>" + "Id" + "</td><td>" + data.city.id + "</td></tr>";
-        out += "<tr><td>" + "Name" + "</td><td>" + data.city.name + "</td></tr>";
-        out += "<tr><td>" + "Country" + "</td><td>" + data.city.country + "</td></tr>";
-        out += "<tr><td>" + "Longitude" + "</td><td>" + data.city.coord.lon + "</td></tr>";
-        out += "<tr><td>" + "Latitude" + "</td><td>" + data.city.coord.lat + "</td></tr>";
-        out += "</table>";
-        document.getElementById("ready to party").innerHTML = out;
+        if (errorcode == 'ZERO_RESULTS'){
+            document.getElementById("codetest").innerHTML = "ERROR! Sorry address doesn't exist";
+        } else if (errorcode == 'REQUEST_DENIED'){
+            document.getElementById("codetest").innerHTML = "ERROR! Your request was denied";
+        } else if (errorcode == 'UNKNOWN_ERROR'){
+            document.getElementById("codetest").innerHTML = "ERROR! There was an unknown error";
+        } else if (errorcode == 'INVALID_REQUEST'){
+            document.getElementById("codetest").innerHTML = "ERROR! That is an invalid request";
+        } else if (errorcode == 'OVER_QUERY_LIMIT') {
+            document.getElementById("codetest").innerHTML = "ERROR! You are over you query limit";
+        } else if (errorcode == 'OK') {
+            document.getElementById("codetest").innerHTML = "All good";
+        }
 
 
-        //This is the summary list
-        var i;
-        var best = "<br />";
-        for(i = 0; i < data.list.length; i++) {
-            best += "<a onclick='furtherInfo()'>" + "Day " + (i+1) + ": " + "</a>";
-            best += "<h4 id='finegrained'></h4>"
-            best += '<img src="http://openweathermap.org/img/w/'+data.list[i].weather[0].icon +'.png">';
-            best += "<p>" + "Generally we can expect tempritures to reach " + data.list[i].temp.day +
+        //Standard Request given by W3
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                myFunction(xmlhttp.responseText);
+            }
+        }
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+
+
+        function myFunction(response) {
+
+            var data = JSON.parse(response);
+
+            // GOOGLE MAP STUFF
+            var myCenter = new google.maps.LatLng(otherdata.results[0].geometry.location.lat, otherdata.results[0].geometry.location.lng);
+
+            var mapProp = {
+                center: myCenter,
+                zoom: 5,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+            var marker = new google.maps.Marker({
+                position: myCenter,
+            });
+
+            marker.setMap(map);
+
+            document.getElementById("Mapintro").innerHTML = "THis is where you picked";
+
+            //This is the city table
+            var out = "<table>";
+            out += "<tr><td>" + "Id" + "</td><td>" + data.city.id + "</td></tr>";
+            out += "<tr><td>" + "Name" + "</td><td>" + data.city.name + "</td></tr>";
+            out += "<tr><td>" + "Country" + "</td><td>" + data.city.country + "</td></tr>";
+            out += "<tr><td>" + "Longitude" + "</td><td>" + data.city.coord.lon + "</td></tr>";
+            out += "<tr><td>" + "Latitude" + "</td><td>" + data.city.coord.lat + "</td></tr>";
+            out += "</table>";
+            document.getElementById("ready to party").innerHTML = out;
+
+
+            //This is the summary list
+            var i;
+            var best = "<br />";
+            for (i = 0; i < data.list.length; i++) {
+                best += "<a onclick='furtherInfo(" + (i + 1) + ")'>" + "Day " + (i + 1) + ": " + "</a>";
+                best += "<h4 id= class" + (i + 1) + "></h4>"
+                best += '<img src="http://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png">';
+                best += "<p>" + "Generally we can expect tempritures to reach " + data.list[i].temp.day +
                     " during the day." + " and " + data.list[i].temp.night + " during the night. " +
                     "<br />" + "In terms of cloudiness it will be " + data.list[i].clouds + "%." +
                     " On this day we can expect to see: " + data.list[i].weather[0].main + ", " +
                     data.list[i].weather[0].description + "</p>";
-            best += "<ul>";
-            best += "<li>" + "The minimum temperature is day is: " + data.list[i].temp.min + "</li>";
-            best += "<li>" + "The maximum temperature for this day is: " + data.list[i].temp.max + "</li>";
-            best += "<li>" + "The predicted rainfall for this day is: " + " DONT HAVE YET " + "</li>";
-            best += "</ul>";
+                best += "<ul>";
+                best += "<li>" + "The minimum temperature is day is: " + data.list[i].temp.min + "</li>";
+                best += "<li>" + "The maximum temperature for this day is: " + data.list[i].temp.max + "</li>";
+                best += "<li>" + "The predicted rainfall for this day is: " + data.list[i].rain + "</li>";
+                best += "</ul>";
+            }
+            document.getElementById("summary").innerHTML = best;
+
+
+            //For loop to add the table from the array
+
+            var other = "<table style='float:left'>";
+            other += "<tr><td>" + "PRESSURE" + "</td></tr>";
+            for (i = 0; i < data.list.length; i++) {
+                other += "<tr><td>" + data.list[i].pressure + "</td></tr>";
+            }
+            other += "</table>";
+
+            var other2 = "<table style='float:left'>";
+            other2 += "<tr><td>" + "HUMIDITY" + "</td></tr>";
+            for (i = 0; i < data.list.length; i++) {
+                other2 += "<tr><td>" + data.list[i].humidity + "</td></tr>";
+            }
+            other2 += "</table>";
+
+            var other3 = "<table style='float:left'>";
+            other3 += "<tr><td>" + "WIND SPEED" + "</td></tr>";
+            for (i = 0; i < data.list.length; i++) {
+                other3 += "<tr><td>" + data.list[i].speed + "</td></tr>";
+            }
+            other3 += "</table>";
+
+            //document.getElementById("ready to party").innerHTML = "Here is the city information" + "<br /><br />"  + out;
+            //document.getElementById("summary").innerHTML = best;
+            document.getElementById("tabletest").innerHTML = other;
+            document.getElementById("tabletest2").innerHTML = other2;
+            document.getElementById("tabletest3").innerHTML = other3;
+
+
+            return data;
         }
-        document.getElementById("summary").innerHTML = best;
-
-
-        //For loop to add the table from the array
-
-        var other = "<table style='float:left'>";
-        other += "<tr><td>" + "PRESSURE (ground, sea)" + "</td></tr>";
-        for(i = 0; i < data.list.length; i++) {
-            other +=  "<tr><td>" + data.list[i].pressure + "</td></tr>";
-        }
-        other += "</table>";
-
-        var other2 = "<table style='float:left'>";
-        other2 += "<tr><td>" + "HUMIDITY" + "</td></tr>";
-        for(i = 0; i < data.list.length; i++) {
-            other2 +=  "<tr><td>" + data.list[i].humidity + "</td></tr>";
-        }
-        other2 += "</table>";
-
-        var other3 = "<table style='float:left'>";
-        other3 += "<tr><td>" + "WIND SPEED" + "</td></tr>";
-        for(i = 0; i < data.list.length; i++) {
-            other3 +=  "<tr><td>" + data.list[i].speed + "</td></tr>";
-        }
-        other3 += "</table>";
-
-        var checkbx = "<input type=\"checkbox\" name=\"c1\" onclick=\"showMe('table1')\">Show Hide Checkbox" +
-        "<input type=\"checkbox\" name=\"c1\" onclick=\"showMe('table2')\">Show Hide Checkbox" +
-        "<input type=\"checkbox\" name=\"c1\" onclick=\"showMe('table3')\">Show Hide Checkbox";
-
-        //document.getElementById("ready to party").innerHTML = "Here is the city information" + "<br /><br />"  + out;
-        //document.getElementById("summary").innerHTML = best;
-        document.getElementById("checkboxes").innerHTML = checkbx;
-        document.getElementById("table1").innerHTML = other;
-        document.getElementById("table2").innerHTML = other2;
-        document.getElementById("table3").innerHTML = other3;
-
-
-        return data;
     }
 
 
@@ -127,13 +163,14 @@ function testResults (form) {
 }
 
 
-function furtherInfo () {
+function furtherInfo (choice) {
+
+    //var numberChoice = 3;
 
     var xmlhttp = new XMLHttpRequest();
     var location = 'London,us';
     var units = 'metric';
 
-    var xmlhttp = new XMLHttpRequest();
     var url = 'http://api.openweathermap.org/data/2.5/forecast?q='+location+'&mode=json&appid=dab71e5e8eb47351af0a7bdb5bcac16b&units='+units;
 
     //Standard Request given by W3
@@ -158,8 +195,6 @@ function furtherInfo () {
         var data3 = '';
         var data4 = '';
         var data5 = '';
-
-        var numberChoice = 3;
 
         //Get the number for the day
 
@@ -250,16 +285,16 @@ function furtherInfo () {
 
             }
 
-            if (numberChoice == 1){
-                document.getElementById("finegrained").innerHTML = data1;
-            } else if (numberChoice == 2){
-                document.getElementById("finegrained").innerHTML = data2;
-            } else if (numberChoice == 3) {
-                document.getElementById("finegrained").innerHTML = data3;
-            } else if (numberCHoice == 4){
-                document.getElementById("finegrained").innerHTML = data4;
-            } else if (numberCHoice == 4){
-                document.getElementById("finegrained").innerHTML = data5;
+            if (choice == 1){
+                document.getElementById("class1").innerHTML = data1;
+            } else if (choice == 2){
+                document.getElementById("class2").innerHTML = data2;
+            } else if (choice == 3) {
+                document.getElementById("class3").innerHTML = data3;
+            } else if (choice == 4){
+                document.getElementById("class4").innerHTML = data4;
+            } else if (choice == 5){
+                document.getElementById("class5").innerHTML = data5;
             }
 
 
---
+++ newtitl.js
@@ -0,0 +1,4 @@
+/**
+ * Created by kevinfitzpatrick on 07/04/2016.
+ */
+'https://maps.googleapis.com/maps/api/geocode/json?parameters address key';
